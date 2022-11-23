cd32cda641f37bd9404e95907a856950d7d9effb
Christian Riaño <christianrv.dev@gmail.com>
2022-11-23 09:00:00 -0500

    Set functions for building the menu of options

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,21 +1,40 @@
 const colors = require('colors');
+const { resolve } = require('path');
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const showMenu = () => {
-  console.clear();
+const showMenu = () =>
+  new Promise(resolve => {
+    console.clear();
 
-  console.log('================='.green);
-  console.log(' Enter an option  '.green);
-  console.log('=================\n'.green);
+    console.log('================='.green);
+    console.log(' Enter an option  '.green);
+    console.log('=================\n'.green);
 
-  console.log(`${colors.green(1)}. Create a task`);
-  console.log(`${colors.green(2)}. List tasks`);
-  console.log(`${colors.green(3)}. List tasks completed`);
-  console.log(`${colors.green(4)}. List tasks pending`);
-  console.log(`${colors.green(5)}. Complete tasks`);
-  console.log(`${colors.green(6)}. Delete a task`);
-  console.log(`${colors.green(7)}. Exit \n`);
-};
+    console.log(`${colors.green(1)}. Create a task`);
+    console.log(`${colors.green(2)}. List tasks`);
+    console.log(`${colors.green(3)}. List tasks completed`);
+    console.log(`${colors.green(4)}. List tasks pending`);
+    console.log(`${colors.green(5)}. Complete tasks`);
+    console.log(`${colors.green(6)}. Delete a task`);
+    console.log(`${colors.green(7)}. Exit \n`);
+
+    readline.question(`Enter an option ${'1-7'.blue}: `, opt => {
+      resolve(opt);
+    });
+  });
+
+const doPause = () =>
+  new Promise(resolve => {
+    readline.question(`\nPress ${'ENTER'.green} to continue\n`, opt => {
+      resolve();
+      readline.close();
+    });
+  });
 
 module.exports = {
   showMenu,
+  doPause,
 };
