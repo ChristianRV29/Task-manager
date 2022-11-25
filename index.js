87f53cfd50cf8cb194a89ef45477204bb9aeec76
Christian Riaño <christianrv.dev@gmail.com>
2022-11-25 18:46:13 -0500

    Implement the new features into menu

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 'use strict';
+require('colors');
 
-const { showInquirerMenu, doPause } = require('./src/utils/inquirer');
-const colors = require('colors');
+const Tasks = require('./src/models/tasks');
+const {
+  showInquirerMenu,
+  doPause,
+  readInput,
+} = require('./src/utils/inquirer');
 
 const main = async () => {
+  const tasks = new Tasks();
   let hasLeft = false;
   do {
     const { option } = await showInquirerMenu();
+
     hasLeft = Boolean(option === 7);
+
+    switch (option) {
+      case 1:
+        const desc = await readInput("Task's description");
+        tasks.createTask(desc);
+        break;
+      case 2:
+        console.log(tasks._list);
+        break;
+      default:
+        null;
+    }
 
     if (!hasLeft) await doPause();
   } while (!hasLeft);
 
-  console.log('\nThe program has finished successfully!'.green);
+  console.log('\nThe program has finished successfully!\n'.green);
 };
 
 main();
