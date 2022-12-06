a517a5f956cb3c5e5f50dfc594427b23d3257309
Christian Riaño <christianrv.dev@gmail.com>
2022-12-05 21:01:39 -0500

    Update function from tasks class

--- src/models/tasks.js
+++ src/models/tasks.js
@@ -1,5 +1,7 @@
-const Task = require('./task');
+const colors = require('colors');
 
+const Task = require('./task');
+const { readData } = require('../utils');
 class Tasks {
   _list = {};
 
@@ -12,13 +14,33 @@ class Tasks {
     this._list[task.id] = task;
   }
 
-  getListAsArray() {
-    const taskList = Object.keys(this._list).map(key => {
-      const task = this._list[key];
-      return task;
-    });
+  async getTasksFromDB() {
+    try {
+      const { data = [] } = await readData();
+      if (data && data.length > 0) {
+        const tasks = JSON.parse(data);
 
-    return taskList;
+        (tasks || []).forEach(task => {
+          this._list[task.id] = task;
+        });
+      }
+    } catch (err) {
+      console.log(colors.yellow(err?.message || err));
+      return null;
+    }
+  }
+
+  getTaskData() {
+    return Object.keys(this._list).map(id => this._list[id]) || [];
+  }
+
+  listAllTasks() {
+    Object.keys(this._list).forEach((id, index) => {
+      const { description, isCompleted } = this._list[id];
+      const message = `${index + 1}.${description}\n`;
+
+      console.log(isCompleted ? colors.green(message) : colors.blue(message));
+    });
   }
 }
 
